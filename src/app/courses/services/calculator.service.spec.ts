32e15c3ef302e93ef5616f304578f67ffbffc120
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2020-07-20 18:16:52 -0300

    first commit

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,45 @@
+import { CalculatorService } from './calculator.service';
+import { TestBed } from '@angular/core/testing';
+import { LoggerService } from './logger.service';
+
+describe('CalculatorService', () => {
+
+  let calculator: CalculatorService;
+  let loggerSpy: any;
+
+  beforeEach(() => {
+
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {provide: LoggerService, useValue: loggerSpy}
+      ]
+    });
+
+    calculator = TestBed.inject(CalculatorService);
+
+  });
+
+  it('should add two numbers', () => {
+
+    const result = calculator.add(2, 2);
+
+    expect(result).toBe(4);
+
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
+  });
+
+  it('should substract two numbers', () => {
+
+    const result = calculator.subtract(2, 2);
+
+    expect(result).toBe(0);
+
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
+  });
+
+});
